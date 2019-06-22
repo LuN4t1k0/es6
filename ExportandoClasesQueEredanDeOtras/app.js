693d450ea5d352f1eb7b7415d5135a5800c57add
LuN4t1k0 <cristian.venegas@materoceano.com>
2019-06-21 22:49:49 -0400

    Clase 06

--- ExportandoClasesQueEredanDeOtras/app.js
+++ ExportandoClasesQueEredanDeOtras/app.js
@@ -1,6 +1,8 @@
 import Tarea from "./tareas.js";
+import ComprasPendientes from "./compras.js";
 
 const tarea1 = new Tarea("Aprender JavaScript", "Urgente");
+const compra1 = new ComprasPendientes("Aprender ReactJS", "urgente", "x3");
 
-console.log(tarea1);
 console.log(tarea1.mostrar());
+compra1.mostrar();
